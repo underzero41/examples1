dd4b168a6d560b75b23f49d24a2b725ed55317b9
1chello <underzero@bk.ru>
2023-01-16 16:53:56 +0300

    example9

---
+++ example_console9/Program.cs
@@ -0,0 +1,17 @@
+﻿//нахождение индекса по запрашиваемому числу
+
+int[] array = {11,21,61,14,51,61,17,18};
+int n = array.Length;
+int find = 61;
+int index = 0;
+while(index<n)
+{
+    
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
