5b7f296d9d7661a8d456d6641021e13ea49cc7bb
1chello <underzero@bk.ru>
2023-01-30 16:56:07 +0300

    add lection4

---
+++ example_console14/Program.cs
@@ -0,0 +1,12 @@
+﻿//рекурсия через факториал
+
+double Factorial (double n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (double i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
